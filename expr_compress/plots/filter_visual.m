6ac138cdc804b4420f79aa082781bc7440d19236
Wojciech Zaremba <woj.zaremba@gmail.com>
2014-01-30 23:06:43 -0500

    Little bit more in experimental section.

--- expr_compress/plots/filter_visual.m
+++ expr_compress/plots/filter_visual.m
@@ -5,6 +5,7 @@ sizes = [7, 11];
 names = {'matthew', 'alex'};
 for k = 1:length(names)
     name = names{k};
+    s = sizes(k);
     json_old = ParseJSON(sprintf('plans/imagenet_%s.txt', name));
     json = {};
     json{1} = json_old{1};
@@ -23,22 +24,22 @@ for k = 1:length(names)
     Q = permute([W; Wapprox], [4, 1, 2, 3]);
     Q = Q(:, :);
     Q = min(Q, [], 2) - 1e-4;
-    Q = repmat(reshape(Q, [1, 1, 1, 3]), [96, sizes(k), sizes(k), 1]);
+    Q = repmat(reshape(Q, [1, 1, 1, 3]), [96, s, s, 1]);
     W = W - Q; 
     Wapprox = Wapprox - Q; 
     Q = permute([W; Wapprox], [4, 1, 2, 3]);
     Q = Q(:, :);
     Q = max(Q, [], 2) + 1e-4;
-    Q = repmat(reshape(Q, [1, 1, 1, 3]), [96, sizes(k), sizes(k), 1]);
+    Q = repmat(reshape(Q, [1, 1, 1, 3]), [96, s, s, 1]);
     W = W ./ Q;
     Wapprox = Wapprox ./ Q;
 
-    img = ones(sizes(k) * 12, 2 * 10 * sizes(k), 3);
+    img = ones((s + 1) * 12 - 1, 2 * 8 * (s + 6) - 10, 3);
     for i = 1:12
         for j = 1:8
-            idx = (j - 1) * 8 + i;
-            img(((i - 1) * 8 + 2):((i - 1) * 8 + 1 + sizes(k)), ((j - 1) * 2 * 10 + 3):((j - 1) * 2 * 10 + 2 + sizes(k)), :) = squeeze(W(idx, :, :, :));
-            img(((i - 1) * 8 + 2):((i - 1) * 8 + 1 + sizes(k)), ((j - 1) * 2 * 10 + 4 + sizes(k)):((j - 1) * 2 * 10 + 3 + 2 * sizes(k)), :) = squeeze(Wapprox(idx, :, :, :));
+            idx = (j - 1) * 12 + i;
+            img(((i - 1) * (s + 1) + 1):((i - 1) * (s + 1)  + s), ((j - 1) * 2 * (s + 6) + 1):((j - 1) * 2 * (s + 6) + s), :) = squeeze(W(idx, :, :, :));
+            img(((i - 1) * (s + 1) + 1):((i - 1) * (s + 1)  + s), ((j - 1) * 2 * (s + 6) + 3 + s):((j - 1) * 2 * (s + 6) + 2 + 2 * s), :) = squeeze(Wapprox(idx, :, :, :));
         end
     end
     imagesc(img);
